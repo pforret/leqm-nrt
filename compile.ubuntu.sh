a3d05e260bb4b4eefdaffb4f86bfdf28ccfcb7c8
Peter Forret <peter@forret.com>
2024-05-20 21:35:55 +0200

    Update compile.ubuntu.sh

--- compile.ubuntu.sh
+++ compile.ubuntu.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 # Compile on Ubuntu (WSL)
-sudo apt install libsndfile1 libsndfile1-de
+sudo apt update
+sudo apt install libsndfile1 libsndfile1-dev
 sudo apt install autoconf
+rm -fr missing
 autoreconf -f -i
 ./configure
 make
